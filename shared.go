83387ca585c52e74b125124e068acfb18aebe437
Bankole Ojo <ojobankole@gmail.com>
2022-08-02 01:39:01 +0100

    updated working directory for this app under snapcraft

--- shared.go
+++ shared.go
@@ -1,31 +1,36 @@
 package main
 
 import (
-  "os"
-  "path/filepath"
-  "math/rand"
-  "time"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
-
 
 func GetRootPath() (string, error) {
 	hd, err := os.UserHomeDir()
 	if err != nil {
 		return "", err
 	}
-  dd := filepath.Join(hd, "Sites115")
+
+	dd := os.Getenv("SNAP_USER_COMMON")
+
+	if strings.HasPrefix(dd, filepath.Join(hd, "snap", "go")) || dd == "" {
+		dd = filepath.Join(hd, "Sites115")
+		os.MkdirAll(dd, 0777)
+	}
 
 	return dd, nil
 }
 
-
 func UntestedRandomString(length int) string {
-  var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
-  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+	var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
-  b := make([]byte, length)
-  for i := range b {
-    b[i] = charset[seededRand.Intn(len(charset))]
-  }
-  return string(b)
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
 }
